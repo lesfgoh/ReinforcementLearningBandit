78d7b9e6d07295b60437a063027dee9680828242
lesfgoh <htho002@e.ntu.edu.sg>
2024-08-21 17:56:15 +0800

    no more... no more...

--- bandit.py
+++ bandit.py
@@ -1,7 +1,12 @@
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from flask import Flask, render_template, request, redirect, url_for
 import numpy as np
+import base64
+import io
+import statistics
 
 app = Flask(__name__)
 
@@ -12,7 +17,7 @@ class MultiArmedBandit:
         self.arms = arms
         self.dist = distribution
         self.params= [self.generate_params(dist) for dist in distribution]
-        self.results = [[] for _ in range(5)]
+        self.results = [[] for _ in range(arms)]
     
 
     def generate_params(self, dist):
@@ -26,7 +31,7 @@ class MultiArmedBandit:
         param = self.params[arm]
         match dist:
             case "normal":
-                self.results[arm] += [[np.random.normal(*param)]]
+                self.results[arm] += [np.random.normal(*param)]
             case "beta":
                 self.results[arm] += [np.random.beta(*param)]
             case "uniform":
@@ -36,16 +41,39 @@ class MultiArmedBandit:
 
     def reset(self):
         self.params = [self.generate_params(dist) for dist in self.dist]
-        self.results = [[] for _ in range(5)]
+        self.results = [[] for _ in range(self.arms)]
 
-
-# params = [(1, 1), (2, 1), (2, 5), (0, 1), 0.7]
-
+    def generate_average(self):
+        print(x for x in self.results)
+        return [statistics.mean(x) if x else [0] for x in self.results]
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', arms=range(bandit.arms), params=bandit.params, rewards = bandit.results, dist = bandit.dist)
+    # To generate the scatterplot:
+    img = io.BytesIO()
+    plt.figure(figsize=(20, 7))
+    
+    for i, rewards in enumerate(bandit.results):
+        x_values = [i] *len(rewards)
+        plt.scatter(x_values, rewards, label=f'Arm {i+1}')
+        print(i, rewards)
+
+    plt.xlabel('Pulls')
+    plt.ylabel('Reward')
+    plt.title('Rewards Over Time for Each Arm')
+    plt.legend()
+
+    plt.xticks(ticks=range(bandit.arms), labels=range(1, bandit.arms + 1))
+
+    plt.savefig(img, format='png')
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode('utf8')
+    plt.close()
+
+    return render_template('index.html', arms=range(bandit.arms), 
+                           params=bandit.params, rewards = bandit.results, 
+                           dist = bandit.dist, plot_url = plot_url, average = bandit.generate_average())
 
 @app.route('/pull/<int:arm>', methods=['GET'])
 def pull(arm):
